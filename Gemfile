0f450d4e12663efad3abdda7a4196ddeb2af3709
syy <ns.28.baske@gmail.com>
2022-05-25 12:54:51 +0900

    initial commit.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,12 @@
 source 'https://rubygems.org'
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
 gem 'sinatra'
 gem 'puma'
+gem 'mysql2'
+gem 'activerecord'
+gem 'rake'
+gem 'sinatra-activerecord'
+gem 'pry'
+gem 'bcrypt'
--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require 'sinatra'
- 
+require 'bundler'
+Bundler.require
+require 'rss'
+
+ActiveRecord::Base.configurations = YAML.load_file('database.yml')
+ActiveRecord::Base.establish_connection(:development)
+
+class User <ActiveRecord::Base
+  validates_presence_of :name
+ # validates_presence_of :url
+end
+
+
 get '/' do
   erb :index
 end
 post '/recieve' do
+  @name = params[:name]
   erb :recieve
 end
 get 'admin' do
